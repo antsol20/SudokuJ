bdd74700dad08a089713bcb24924705e66abf93e
antsol20 <antsol20@hotmail.com>
2021-01-02 22:47:32 +0000

    gui update

--- src/SudokuJ.java
+++ src/SudokuJ.java
@@ -3,38 +3,28 @@ import java.awt.*;
 
 public class SudokuJ {
 
-    public static void main(String[] args) {
-        String boardTxt = """
-                0,0,1,8,2,5,0,0,0
-                7,9,2,0,0,0,8,6,0
-                0,0,5,6,0,7,0,0,2
-                5,0,0,0,0,0,0,0,0
-                1,0,0,0,3,0,0,2,6
-                0,0,0,9,6,0,0,0,4
-                0,0,0,0,0,9,6,3,0
-                0,1,6,0,8,0,0,5,7
-                2,0,3,1,5,6,9,4,8
-                """;
+    private final static JTextField[] textBoxes = new JTextField[81];
 
-        SudokuJSolve s = new SudokuJSolve(boardTxt);
-        s.solve();
-
-        JTextField[] textBoxes = new JTextField[81];
-
+    private static void createGui() {
+        String loc = "C:\\Users\\Antony\\Documents\\Source_code\\SudokuJ\\src\\ico.png";
+        ImageIcon img = new ImageIcon(loc);
         JFrame f = new JFrame("SudokuJ");
+        f.setIconImage(img.getImage());
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setSize(600, 600);
         f.setLocation(300, 200);
         f.setLayout(new GridLayout(9, 9));
 
 
+
         JPanel p = new JPanel();
         p.setLayout(new GridLayout(9, 9));
+        int pointer = 0;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-
-                textBoxes[i * j] = new JTextField();
-                p.add(textBoxes[i * j]);
+                textBoxes[pointer] = new SudokuJGuiBox();
+                p.add(textBoxes[pointer]);
+                pointer++;
             }
         }
 
@@ -49,7 +39,43 @@ public class SudokuJ {
 
         m1.addActionListener(e -> {
 
+
         });
 
     }
+
+    public static void main(String[] args) {
+        String boardTxt = """
+                0,0,1,8,2,5,0,0,0
+                7,9,2,0,0,0,8,6,0
+                0,0,5,6,0,7,0,0,2
+                5,0,0,0,0,0,0,0,0
+                1,0,0,0,3,0,0,2,6
+                0,0,0,9,6,0,0,0,4
+                0,0,0,0,0,9,6,3,0
+                0,1,6,0,8,0,0,5,7
+                2,0,3,1,5,6,9,4,8
+                """;
+
+
+        SudokuJSolve s = new SudokuJSolve(boardTxt);
+        // s.solve();
+        createGui();
+        populateGrid(boardTxt);
+
+    }
+
+    private static void populateGrid(String grid) {
+
+        int pointer = 0;
+        String[] rows = grid.split("\n");
+        for (int i = 0; i < 9; i++) {
+            String[] vals = rows[i].split(",");
+            for (int j = 0; j < 9; j++) {
+                textBoxes[pointer].setText(vals[j]);
+                pointer++;
+            }
+        }
+    }
+
 }
---
+++ src/SudokuJGuiBox.java
@@ -0,0 +1,11 @@
+import javax.swing.JTextField;
+import java.awt.*;
+
+public class SudokuJGuiBox extends JTextField{
+
+    public SudokuJGuiBox() {
+        setHorizontalAlignment(JTextField.CENTER);
+        setFont(new Font("SansSerif", Font.BOLD, 24));
+        setEditable(false);
+    }
+}
